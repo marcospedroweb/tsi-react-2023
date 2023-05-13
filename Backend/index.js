78230fc908e509ad6b347eec14608d8cf9b108de
MARCOS PEDRO ALVES <marcos.palves4@CAS474678C148A.local>
2023-05-12 21:17:46 -0300

    Final da aula

--- Backend/index.js
+++ Backend/index.js
@@ -12,11 +12,46 @@ const client = new MongoClient(uri, {
 });
 
 const db = client.db('Pizzaria');
+
 async function insertElement() {
   const pizzas = db.collection('pizzas');
-  const pizza = { nome: '4 Queijos', qtdPedacos: '8' };
+  const documents = [
+    { nome: '3 Queijos', qtdPedacos: '8' },
+    { nome: 'Baiana', qtdPedacos: '8' },
+    { nome: 'Frango Catupiri', qtdPedacos: '8' },
+    { nome: 'Calabresa', qtdPedacos: '8' },
+  ];
+  const result = await pizzas.insertMany(documents);
+  let ids = result.insertedIds;
+  console.log(ids);
+  for (let id of Object.values(ids)) {
+    console.log(`A pizza inserida foi: ${id}`);
+  }
 }
 
+// insertElement();
+async function getData() {
+  const pizzas = await db.collection('pizzas').find({ nome: '4 Queijos' });
+  for await (let pizza of pizzas) {
+    console.log(pizza);
+  }
+}
+// getData();
+
+async function updateValue() {
+  const pizzas = db.collection('pizzas');
+  await pizzas.updateOne({ nome: '4 Queijos' }, { $set: { qtdPedacos: 12 } });
+  getData();
+}
+
+// updateValue();
+async function deleteData() {
+  const pizzas = db.collection('pizzas');
+  const pizza = await pizzas.findOne({ nome: '4 Queijos' });
+  pizzas.deleteOne(pizza);
+  getData();
+}
+deleteData();
 // async function run() {
 //   try {
 //     // Connect the client to the server	(optional starting in v4.7)
